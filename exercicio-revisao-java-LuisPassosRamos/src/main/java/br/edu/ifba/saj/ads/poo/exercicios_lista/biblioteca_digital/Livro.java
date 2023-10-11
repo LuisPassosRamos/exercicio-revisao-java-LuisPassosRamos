f03426270fe933ab1965b445e860667790345de0
LuisPassosRamos <129339010+LuisPassosRamos@users.noreply.github.com>
2023-10-11 23:07:57 +0000

    changes

--- exercicio-revisao-java-LuisPassosRamos/src/main/java/br/edu/ifba/saj/ads/poo/exercicios_lista/biblioteca_digital/Livro.java
+++ exercicio-revisao-java-LuisPassosRamos/src/main/java/br/edu/ifba/saj/ads/poo/exercicios_lista/biblioteca_digital/Livro.java
@@ -2,7 +2,12 @@ package br.edu.ifba.saj.ads.poo.exercicios_lista.biblioteca_digital;
 
 public class Livro{
     
-    Categoria cat; 
-    Autores autor;
+    ArrayList<Categoria> categorias; 
+    ArrayList<Autor> autores;
+
+    livro(){
+        this.categorias = new ArrayList();
+        this.autores = new ArrayList();
+    }
 
 }
