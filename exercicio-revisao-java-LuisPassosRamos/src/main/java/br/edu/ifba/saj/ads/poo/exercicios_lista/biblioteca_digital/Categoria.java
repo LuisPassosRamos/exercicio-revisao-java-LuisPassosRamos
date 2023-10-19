14fe43b1d83c8f8358ae9519b0307fbe12cd0dd8
LuisPassosRamos <129339010+LuisPassosRamos@users.noreply.github.com>
2023-10-19 14:59:45 +0000

    changes

--- exercicio-revisao-java-LuisPassosRamos/src/main/java/br/edu/ifba/saj/ads/poo/exercicios_lista/biblioteca_digital/Categoria.java
+++ exercicio-revisao-java-LuisPassosRamos/src/main/java/br/edu/ifba/saj/ads/poo/exercicios_lista/biblioteca_digital/Categoria.java
@@ -1,5 +1,34 @@
 package br.edu.ifba.saj.ads.poo.exercicios_lista.biblioteca_digital;
 
-public class Categoria{
-    
+import java.util.List;
+import java.util.ArrayList;
+
+public class Categoria {
+    private String nomeCategoria;
+    private ArrayList<Livro> livro;
+
+    public Categoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+
+    }
+
+    public ArrayList<Livro> getLivro() {
+        return livro;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void novoLivro(Livro livro) {
+        this.livro.add(livro);
+    }
+
+    public Livro buscaLivro(String livroNome) {
+        for (Livro livro : this.livro) {
+            if (livro.getnomeLivro().equals(livroNome)) {
+                return this.livro;
+            }
+        }
+    }
 }
--- exercicio-revisao-java-LuisPassosRamos/src/main/java/br/edu/ifba/saj/ads/poo/exercicios_lista/biblioteca_digital/Livro.java
+++ exercicio-revisao-java-LuisPassosRamos/src/main/java/br/edu/ifba/saj/ads/poo/exercicios_lista/biblioteca_digital/Livro.java
@@ -1,13 +1,25 @@
 package br.edu.ifba.saj.ads.poo.exercicios_lista.biblioteca_digital;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Livro{
     
-    private ArrayList<Categoria> categorias; 
+    private Categoria categorias; 
     private Autor autor;
+    private String nomeLivro;
 
-    public livro(Autor autor){
+    public Livro(Autor autor, String nomeLivro, Categoria categoria){
         this.autor = autor;
-        this.categorias = new ArrayList();
+        this.nomeLivro = nomeLivro;
+        this.categorias = categoria;
+        novoLivro(this);
     }
 
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public String toString(){
+        return "[Nome do livro: "+this.nomeLivro+"]\n[Categoria do livro: "+this.categorias.getName()+"]\n [Autor: "+autor+"]";
+    }
 }
