f4dfe148f4f92c0b147d01bd68f7a8943a754b6e
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2023-11-16 21:35:22 -0300

    changes

--- src/main/java/br/edu/ifba/saj/ads/poo/exercicios_lista/loja_roupas/Loja.java
+++ src/main/java/br/edu/ifba/saj/ads/poo/exercicios_lista/loja_roupas/Loja.java
@@ -3,8 +3,9 @@ package br.edu.ifba.saj.ads.poo.exercicios_lista.loja_roupas;
 public class Loja {
     public static void main(String[] args) {
         Departamento departamentoMasc = new Departamento("Masculino");
+        ArrayList<Roupa> roupas = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            departamentoMasc.criarRoupa("camisa", "vermelho", Tamanho.M, i+1);
+            departamentoMasc.addRoupa(new Roupa("camisa", departamentoMasc, "vermelho", Tamanho.M, i+1));
         }
         Carrinho carrinho1 = new Carrinho();
         carrinho1.addRoupa(new Roupa ("camisa",departamentoMasc, "vermelho", Tamanho.M, 1));
