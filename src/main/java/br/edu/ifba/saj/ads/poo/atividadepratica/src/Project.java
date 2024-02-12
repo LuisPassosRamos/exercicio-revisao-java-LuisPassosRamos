1f6f5cfa5a04217e904bc0d70444a0dbd5f673ed
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2024-02-12 11:12:30 -0300

    changes

--- src/main/java/br/edu/ifba/saj/ads/poo/atividadepratica/src/Project.java
+++ src/main/java/br/edu/ifba/saj/ads/poo/atividadepratica/src/Project.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Project extends Entity{
+public class Project extends Entity {
     private List<Human> listWorkers;
     private List<Inspection> listInspections;
     private String nameProject;
@@ -11,71 +11,55 @@ public class Project extends Entity{
     private Human arquitectResponsible;
     private Human engineerResponsible;
 
-
-    public Project(int id, String nameProject){
+    public Project(int id, String nameProject) {
         super(id);
         this.stageProject = new ArrayList<>();
-        this.stageProject.add(new Stage(1, "Planning"));        
+        this.stageProject.add(new Stage(1, "Planning"));
         this.nameProject = nameProject;
     }
 
-    public Project(int id, String nameProject, Human arquitectResponsible, Human engineerResponsible){
+    public Project(int id, String nameProject, Human arquitectResponsible, Human engineerResponsible) {
         super(id);
         this.stageProject = new ArrayList<>();
-        this.stageProject.add(new Stage(1, "Planning"));        
+        this.stageProject.add(new Stage(1, "Planning"));
         this.nameProject = nameProject;
         setArquitectResponsible(arquitectResponsible);
         setEngineerResponsible(engineerResponsible);
     }
 
-    public void taskHiring(String nameWorker, int ageWorker, int numberWorker){
+    public void taskHiring(String nameWorker, int ageWorker, int numberWorker) {
         Human newWorker = new Human(nameWorker, listWorkers.size(), "Worker", numberWorker);
-        if (!this.listWorkers.contains(newWorker)){
+        if (!this.listWorkers.contains(newWorker)) {
             this.listWorkers.add(newWorker);
         }
         super.setLastModification();
     }
 
-    public void taskBuying(Item taskItem){
-        //Redireciona para uma pagina na web com o melhor valor do item correspondente
+    public void taskBuying(Item taskItem) {
+        // Redireciona para uma pagina na web com o melhor valor do item correspondente
         super.setLastModification();
     }
 
-    public void taskInspectionSchedule(LocalDateTime inspectionDate){
-        if(inspectionDate.isAfter(LocalDateTime.now())){
-            listInspections.add(new Inspection(listInspections.size(),inspectionDate));
+    public void taskInspectionSchedule(LocalDateTime inspectionDate) {
+        if (inspectionDate.isAfter(LocalDateTime.now())) {
+            listInspections.add(new Inspection(listInspections.size(), inspectionDate));
         }
         super.setLastModification();
     }
 
-    public void setPlanningDone(){
-        stageProject.get(0).setProgressStage(100);
-        stageProject.add(new Stage(2, "Foundation"));
-        super.setLastModification();
-    }
-
-    public void setFoundationDone(){
-        stageProject.get(1).setProgressStage(100);
-        stageProject.add(new Stage(3, "Construction"));
-        super.setLastModification();
-    }
-
-    public void setConstructionDone(){
-        stageProject.get(2).setProgressStage(100);
-        stageProject.add(new Stage(3, "Finishing"));
-        super.setLastModification();
-    }
-    
-    public void setFinishingDone(){
-        stageProject.get(3).setProgressStage(100);
-        super.setLastModification();
+    public void setStageDone(Stage myStage) {
+        stageProject.get(stageProject.size()).setProgressStage(100);
+        if (!stageProject.contains(myStage)) {
+            stageProject.add(myStage);
+            super.setLastModification();
+        }
     }
 
     public void setArquitectResponsible(Human arquitectResponsible) {
         this.arquitectResponsible = arquitectResponsible;
         super.setLastModification();
     }
-    
+
     public void setEngineerResponsible(Human engineerResponsible) {
         this.engineerResponsible = engineerResponsible;
         super.setLastModification();
@@ -92,7 +76,7 @@ public class Project extends Entity{
     public List<Stage> getStageProject() {
         return Collections.unmodifiableList(this.stageProject);
     }
-    
+
     public String getNameProject() {
         return this.nameProject;
     }
@@ -105,5 +89,4 @@ public class Project extends Entity{
         return this.engineerResponsible;
     }
 
-    
 }
